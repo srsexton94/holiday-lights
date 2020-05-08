76f1ebbe6fe19f337150828d6c434330a12e515d
srsexton94 <srsexton94@gmail.com>
2020-05-08 16:34:19 -0400

    Add error message when trying to "play" w/o colors
    Add an error message to `play` function if user tries to press start the
    light display without first selecting a color scheme

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,8 +4,9 @@ const store = require('./store.js')
 const displayTemplate = require('./templates/display.handlebars')
 const helpers = require('./helpers.js')
 
-const sideLength = 15
+const sideLength = 15 // default display side length
 
+// function run on document load, fills display div with "lights"
 const createDisplay = () => {
   const display = []
   for (let i = 0; i < sideLength; i++) {
@@ -16,17 +17,21 @@ const createDisplay = () => {
       row[j] = 0
     }
   })
-  store.display = display
+  store.display = display // records the display array in store object
   const displayHtml = displayTemplate({ rows: display })
-  $('#display').html(displayHtml)
+  $('#display').html(displayHtml) // fills display container w/the lights' html
 }
 
 const setColors = event => {
   store.colors = helpers.colors[event.target.value]
-  console.log(store.colors)
 }
 
 const play = event => {
+  if (!store.colors) {
+    $('#usr-msg').text('Choose a color scheme first!')
+    setTimeout(() => { $('#usr-msg').text('') }, 2000)
+    return
+  }
   $('.btn').toggleClass('hidden')
   // const colors = store.colors
   console.log(store)
